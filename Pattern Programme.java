1a0a6600f416e88cc7531f55ba6c17b55142d524
Priyanka Amarale <53168652+Priyanka-4888@users.noreply.github.com>
2021-10-13 15:46:30 +0530

    First Commit

---
+++ Pattern Programme.java
@@ -0,0 +1,89 @@
+####Program 1
+
+     *
+    **
+   ***
+  ****
+ *****
+ 
+ public static void main(String ar[])
+	{
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+		System.out.println();	
+		}
+	}
+	
+####Program 2
+
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print("* ");
+			}
+			
+		System.out.println();	
+		}
+	}
+
+####Program 3
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+
+public static void main(String ar[])
+	{
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			
+		System.out.println();	
+		}
+	}
+	
+
+####Program 4
+ *****
+  ****
+   ***
+    **
+     *
+	 
+public static void main(String ar[])
+	{
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print("*");
+			}
+		System.out.println();	
+		}
